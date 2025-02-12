541d9470c78d2da94ff973b9693290d009663d4e
TrusTio <erkan.myumyunov@gmail.com>
2025-02-12 12:22:12 +0200

    Stage 2/6 completed

--- Traffic Light Simulator with Java/task/src/traffic/Main.java
+++ Traffic Light Simulator with Java/task/src/traffic/Main.java
@@ -1,5 +1,7 @@
 package traffic;
 
+import java.util.Scanner;
+
 /*
 Stage 1/6 Completed
 Traffic Light Simulator with Java - https://hyperskill.org/projects/288/stages/1500/implement
@@ -7,8 +9,38 @@ Part of Hyperskill's Java Backend Developer (Spring Boot) course.
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the traffic management system!");
-        printMenu();
+
+        System.out.print("Input the number of roads:");
+        int numberOfRoads = scanner.nextInt();
+
+        System.out.print("Input the interval:");
+        int interval = scanner.nextInt();
+
+        boolean quitSelected = false;
+        while (!quitSelected) {
+            printMenu();
+            int input = scanner.nextInt();
+
+            switch (input) {
+                case 1 -> {
+                    numberOfRoads++;
+                    System.out.println("Road added");
+                }
+                case 2 -> {
+                    numberOfRoads--;
+                    System.out.println("Road deleted");
+                }
+                case 3 -> {
+                    System.out.println("System opened");
+                }
+                case 0 -> {
+                    System.out.println("Bye!");
+                    quitSelected = true;
+                }
+            }
+        }
     }
 
     private static void printMenu() {
